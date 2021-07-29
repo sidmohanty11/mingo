90639d7ccdfd2d07a6e34c31e3647ecaca71a669
sidmohanty11 <sidmohanty11@gmail.com>
2021-07-29 16:16:19 +0530

    adding unit test for req.body

--- client/client_core_test.go
+++ client/client_core_test.go
@@ -30,3 +30,64 @@ func TestGetRequestHeaders(t *testing.T) {
 		t.Error("invalid content type received.")
 	}
 }
+
+func TestGetRequestBody(t *testing.T) {
+	// initialization
+	client := client{}
+
+	// execution
+	t.Run("nobodynilresponse", func(t *testing.T) {
+		body, err := client.getReqBody(nil, "")
+
+		// validation
+		if err != nil {
+			t.Error("no error expected here (nil body)")
+		}
+
+		if body != nil {
+			t.Error("no body expected here (nil body)")
+		}
+	})
+
+	t.Run("bodywithjson", func(t *testing.T) {
+		reqBody := []string{"one", "two"}
+		body, err := client.getReqBody(reqBody, "application/json")
+
+		// validation
+		if err != nil {
+			t.Error("no error expected marshaling (json body)")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("no body expected here (marhsal body)")
+		}
+	})
+
+	t.Run("bodywithxml", func(t *testing.T) {
+		reqBody := []string{"one", "two"}
+		body, err := client.getReqBody(reqBody, "application/xml")
+
+		// validation
+		if err != nil {
+			t.Error("no error expected marshaling (xml body)")
+		}
+
+		if string(body) != `<string>one</string><string>two</string>` {
+			t.Error("no body expected here (xml body)")
+		}
+	})
+
+	t.Run("defaultbodythatisjson", func(t *testing.T) {
+		reqBody := []string{"one", "two"}
+		body, err := client.getReqBody(reqBody, "")
+
+		// validation
+		if err != nil {
+			t.Error("no error expected here (default json body)")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("no body expected marshaling (default json body)")
+		}
+	})
+}
